13296652b1942b2fd16f8cd3a42309329732cdb9
HE201151 <romain.blan@hotmail.fr>
2015-03-28 00:58:32 +0100

    BeHackathon

---
+++ behackathon/app/src/main/java/input_fiche.java
@@ -0,0 +1,27 @@
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.romain.behackathon.MainActivity;
+import com.example.romain.behackathon.R;
+
+/**
+ * Created by Romain on 27-03-15.
+ */
+
+
+
+public class input_fiche {
+
+
+    final Button boutonOk = (Button) findViewById(R.id.btnOk);
+    boutonOk.setOnClickListener(new OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+
+        }
+
+
+
+}
